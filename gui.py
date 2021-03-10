c577bbe0b0ca6260c17a16725d0b1ec766c75f82
Arturo Neftalí Ramírez Medina <75585974+arturo-rm@users.noreply.github.com>
2021-03-09 19:10:04 -0600

    Add files via upload

--- gui.py
+++ gui.py
@@ -2,6 +2,18 @@ import módulo2 as m2
 from tkinter import *
 import ctypes
 
+def no_orb():
+ window.destroy()
+ m2.find_npc_duel_no_orb()
+ 
+def npc():
+ window.destroy()    
+ m2.find_npc_duel()
+
+def pvp():
+ window.destroy()
+ m2.pvp_duel()
+
 
 window = Tk()
 
@@ -12,13 +24,13 @@ window.geometry('100x250')
 lbl = Label(window, text="Select Mode:")
 lbl.grid(column=0, row=0)
 
-btn = Button(window, text="NPC duel with no orbs", command = m2.find_npc_duel_no_orb)
+btn = Button(window, text="NPC duel with no orbs", command = no_orb)
 btn.grid(column=0, row=1)
 
-btn2 = Button(window, text="NPC duel with orbs", command = m2.find_npc_duel)
+btn2 = Button(window, text="NPC duel with orbs", command = npc)
 btn2.grid(column=0, row=2)
 
-btn3 = Button(window, text="PvP Duel", command = m2.find_npc_duel)
+btn3 = Button(window, text="PvP Duel", command = pvp)
 btn3.grid(column=0, row=3)
 
  
